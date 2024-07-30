0af01ed035d787f2e87d1bd85e2388fd906b4c04
Seven00321 <richrdpere123@gmail.com>
2024-07-30 17:13:26 -0500

    1er commit del PROYECTO desde DOLORESPATA

---
+++ src/PortfolioApp.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function PortfolioApp() {
+
+
+  return (
+    <h2 className='text-4xl '>Hola mundo</h2>
+  )
+}
+
+export default PortfolioApp;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        "dm-sans": "'DM Sans', sans-serif",
+      },
+
+      colors: {
+        "green-color": "#27AE60",
+        "gray-color": '#828282',
+        "card-color": '#212121',
+        "color-background": '#171717',
+        "menu-color": "#172554"
+      },
+
+      backgroundImage: {
+        "open-menu"
+      }
+    },
+  },
+  plugins: [],
+}
+
